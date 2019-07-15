66abbc083eedc08ee8c5f721415c951c3af6f90a
Aspy <AspySs@users.noreply.github.com>
2019-07-15 23:53:15 +0300

    я ебал

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
 <?php get_header(); ?>
-<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
-
 			<?php
 			// Start the loop.
-			while ( have_posts() ) :
-				the_post();
+			while ( have_posts() ) : the_post();
 
 				/*
 				 * Include the Post-Format-specific template for the content.
@@ -26,22 +17,11 @@
 			endwhile;
 
 			// Previous/next page navigation.
-			the_posts_pagination(
-				array(
+			the_posts_pagination(array(
 					'prev_text'          => __( 'Previous page', 'blog' ),
 					'next_text'          => __( 'Next page', 'blog' ),
 					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'blog' ) . ' </span>',
 				)
-			);
-
-			// If no content, include the "No posts found" template.
-		else :
-			get_template_part( 'content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
+			); ?>
 
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,28 +1,6 @@
 <?php get_header(); ?>
-
-	<div class="entry-content">
-		<?php
-			/* translators: %s: Name of current post */
-			the_content(
-				sprintf(
-					__( 'Continue reading %s', 'blog' ),
-					the_title( '<span class="screen-reader-text">', '</span>', false )
-				)
-			);
-
-			wp_link_pages(
-				array(
-					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'blog' ) . '</span>',
-					'after'       => '</div>',
-					'link_before' => '<span>',
-					'link_after'  => '</span>',
-					'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'blog' ) . ' </span>%',
-					'separator'   => '<span class="screen-reader-text">, </span>',
-				)
-			);
-			?>
-	</div><!-- .entry-content -->
-
+<?php //get_content(); ?>
+<?php //get_index(); ?>
 				<figure>
 
 				<img src="<?php bloginfo('template_directory') ?>/images/blog-logo.png" class="content-img">
